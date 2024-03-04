5d430d8e475b440b13d7c095ff3c4071cfc40719
tomcdj71 <contact.tomc@yahoo.fr>
2024-03-04 12:56:13 +0100

    feat: add `create:base-groups` command

---
+++ src/Command/CreateBaseGroups.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\Group;
+
+#[AsCommand(
+    name: 'create:base-groups',
+    description: 'Create the MediaEase groups in the system',
+)]
+class CreateBaseGroups extends Command
+{
+    public function __construct(private EntityManagerInterface $entityManager, private UserPasswordHasherInterface $userPasswordHasher)
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription('Creates the base groups in the system');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $installLockFile = '/root/.mediaease/.mediaease-install.lock';
+        if (!file_exists($installLockFile)) {
+            $output->writeln('Error: The installation lock file does not exist. Command aborted.');
+
+            return Command::FAILURE;
+        }
+        $groups= [
+            'full',
+            'automation',
+            'media',
+            'remote'
+        ];
+        foreach ($groups as $name) {
+            $group = new Group();
+            $group->setName($name);
+            $this->entityManager->persist($group);
+        }
+        $this->entityManager->flush();
+
+        return Command::SUCCESS;
+    }
+}
