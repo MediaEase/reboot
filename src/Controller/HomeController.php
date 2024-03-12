380a314edeb37610dab54fd777d064c75c0cf162
tomcdj71 <contact.tomc@yahoo.fr>
2024-03-12 18:11:47 +0100

    chore: remove unused imports

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,9 +5,6 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Repository\PreferenceRepository;
-use App\Repository\ServiceRepository;
-use App\Repository\StoreRepository;
-use App\Repository\WidgetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,10 +13,7 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 final class HomeController extends AbstractController
 {
     public function __construct(
-        private WidgetRepository $widgetRepository,
         private PreferenceRepository $preferenceRepository,
-        private ServiceRepository $serviceRepository,
-        private StoreRepository $storeRepository,
     ) {
     }
 
