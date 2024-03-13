f53fc1ac561944b19ec0e0b084cc8cb5041301dc
tomcdj71 <contact.tomc@yahoo.fr>
2024-03-13 16:28:27 +0100

    ui: set current page/tab link color

---
+++ src/Twig/RouteActiveExtension.php
@@ -0,0 +1,32 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class RouteActiveExtension extends AbstractExtension
+{
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('is_active_route', [$this, 'isActiveRoute']),
+        ];
+    }
+
+    public function isActiveRoute(string $routeName): bool
+    {
+        $currentRoute = $this->requestStack->getCurrentRequest()->attributes->get('_route');
+        return $currentRoute === $routeName;
+    }
+}
