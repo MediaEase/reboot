f77d778ddc740d83ce9a60d8a20104c53bcf29e7
tomcdj71 <contact.tomc@yahoo.fr>
2024-03-23 08:44:18 +0100

    ui: enhance php settings

--- src/Form/Setting/PhpType.php
+++ src/Form/Setting/PhpType.php
@@ -7,12 +7,33 @@ namespace App\Form\Setting;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 
 final class PhpType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $formBuilder, array $options): void
     {
+        foreach ($options['data'] as $key => $value) {
+            $formFieldName = str_replace('.', '_', $key);
+            $helpKey = 'form.help.php_' . $key;
+            $fieldOptions = [
+                'required' => false,
+                'data' => $value,
+                'label' => 'forms.php.label.' . $key,
+                'help' => $helpKey,
+                'attr' => [
+                    'class' => 'input',
+                    'placeholder' => $key,
+                ],
+            ];
+            if ($key === 'mail.force_extra_parameters') {
+                $fieldOptions['disabled'] = true;
+            }
+
+            $formBuilder->add($formFieldName, TextType::class, $fieldOptions);
+        }
+
         $formBuilder
             ->add('save', ButtonType::class, [
                 'label' => 'Save',
@@ -25,5 +46,8 @@ final class PhpType extends AbstractType
 
     public function configureOptions(OptionsResolver $optionsResolver): void
     {
+        $optionsResolver->setDefaults([
+            'data_class' => null,
+        ]);
     }
 }
