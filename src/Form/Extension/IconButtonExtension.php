0c49d006b6b299338e56c42961b11666c46778c6
tomcdj71 <contact.tomc@yahoo.fr>
2024-05-24 10:15:44 +0200

    feat: IconButtonExtension
    this adds the hability to create iconed buttons with ease !

---
+++ src/Form/Extension/IconButtonExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class IconButtonExtension extends AbstractTypeExtension
+{
+    public static function getExtendedTypes(): iterable
+    {
+        return [ButtonType::class];
+    }
+
+    public function buildView(FormView $formView, FormInterface $form, array $options): void
+    {
+        $formView->vars['icon_before'] = $options['icon_before'];
+        $formView->vars['icon_after'] = $options['icon_after'];
+        $formView->vars['button_class'] = $options['button_class'];
+        $formView->vars['icon_class'] = $options['icon_class'];
+    }
+
+    public function configureOptions(OptionsResolver $optionsResolver): void
+    {
+        $optionsResolver->setDefaults([
+            'icon_before' => null,
+            'icon_after' => null,
+            'button_class' => null,
+            'icon_class' => 'w-6 h-6 fill-white',
+        ]);
+    }
+}
