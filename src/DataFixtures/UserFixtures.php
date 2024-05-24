71e41bd3579bd513d056db14c9de5954c1e92420
tomcdj71 <contact.tomc@yahoo.fr>
2024-05-24 10:18:45 +0200

    test: use new properties in UserFixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -44,14 +44,19 @@ final class UserFixtures extends Fixture implements DependentFixtureInterface, F
     {
         $user = new User();
         $group = $this->getReference('group-media');
+        $isVerified = (bool)random_int(0, 1);
+        if ($isVerified) {
+            $user->setActivatedAt(new \DateTimeImmutable());
+        }
 
         $user->setUsername($username)
             ->setRoles([$role])
             ->setGroup($group)
             ->setEmail($username.'@example.com')
-            ->setIsVerified(true)
+            ->setIsVerified($isVerified)
             ->setPassword($this->userPasswordHasher->hashPassword($user, 'password'))
-            ->setApiKey(bin2hex(random_bytes(16)));
+            ->setApiKey(bin2hex(random_bytes(16)))
+            ->setRegisteredAt(new \DateTimeImmutable());
 
         $objectManager->persist($user);
         $this->addReference('user-'.$username, $user);
