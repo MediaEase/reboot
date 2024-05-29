ab5976c370df4d4c205bb68ce7c1a0c8e59c7d7c
tomcdj71 <contact.tomc@yahoo.fr>
2024-05-29 09:45:16 +0200

    ui: better ui for the Transcoding page

--- src/Controller/Site/Admin/TranscodingController.php
+++ src/Controller/Site/Admin/TranscodingController.php
@@ -23,7 +23,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/admin', name: 'admin_')]
+#[Route('/settings', name: 'app_settings_extras_')]
 #[IsGranted('ROLE_ADMIN')]
 class TranscodingController extends AbstractController
 {
