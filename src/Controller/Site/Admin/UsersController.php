d6f8f5464605184e2fd487286e2060a23edd8d49
tomcdj71 <contact.tomc@yahoo.fr>
2024-06-09 11:18:55 +0200

    feat: add the CreateUserType form in the modale to create an user

--- src/Controller/Site/Admin/UsersController.php
+++ src/Controller/Site/Admin/UsersController.php
@@ -15,6 +15,8 @@ namespace App\Controller\Site\Admin;
 
 use App\Entity\User;
 use App\Entity\Setting;
+use App\Form\CreateUserType;
+use App\Handler\RegistrationHandler;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,6 +25,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('', name: 'app_settings_')]
 #[IsGranted('ROLE_ADMIN')]
@@ -32,33 +35,32 @@ class UsersController extends AbstractController
         private Security $security,
         private EntityManagerInterface $entityManager,
         private RequestStack $requestStack,
+        private RegistrationHandler $registrationHandler
     ) {
     }
 
-    #[Route('/users', name: 'users_list', methods: ['GET'])]
-    public function list(#[CurrentUser] User $user): Response
+    #[Route('/users', name: 'users_list', methods: ['GET', 'POST'])]
+    public function list(#[CurrentUser] User $user, Request $request): Response
     {
         $users = $this->entityManager->getRepository(User::class)->findAll();
         $settings = $this->entityManager->getRepository(Setting::class)->findLast();
+        $newUser = new User();
+        $createUserForm = $this->createForm(CreateUserType::class, $newUser);
+        $createUserForm->handleRequest($request);
 
-        usort($users, static function ($a, $b): int {
-            $aIsAdmin = in_array('ROLE_ADMIN', $a->getRoles(), true);
-            $bIsAdmin = in_array('ROLE_ADMIN', $b->getRoles(), true);
-            if ($aIsAdmin && !$bIsAdmin) {
-                return -1;
-            }
+        if ($createUserForm->isSubmitted() && $createUserForm->isValid()) {
+            $plainPassword = $createUserForm->get('plainPassword')->getData();
+            $this->registrationHandler->handleRegistration($newUser, $plainPassword, 'admin_creation');
+            $this->addFlash('success', 'User created successfully.');
 
-            if (!$aIsAdmin && $bIsAdmin) {
-                return 1;
-            }
-
-            return strcmp($a->getUsername(), $b->getUsername());
-        });
+            return $this->redirectToRoute('app_settings_users_list');
+        }
 
         return $this->render('pages/users/list/users.html.twig', [
-            'users' => $users,
+            'users' => $this->sortUsers($users),
             'settings' => $settings,
             'user' => $user,
+            'createUserForm' => $createUserForm->createView(),
         ]);
     }
 
@@ -94,5 +96,40 @@ class UsersController extends AbstractController
         $this->addFlash('success', 'User deleted successfully');
 
         return $this->redirectToRoute('app_settings_users_list');
+    }
+
+    /**
+     * Sorts users by role and username.
+     *
+     * @param User[] $users
+     */
+    private function sortUsers(array $users): array
+    {
+        usort($users, static function ($a, $b): int {
+            $aIsAdmin = in_array('ROLE_ADMIN', $a->getRoles(), true);
+            $bIsAdmin = in_array('ROLE_ADMIN', $b->getRoles(), true);
+            if ($aIsAdmin && !$bIsAdmin) {
+                return -1;
+            }
+
+            if (!$aIsAdmin && $bIsAdmin) {
+                return 1;
+            }
+
+            $aUsername = $a->getUsername();
+            $bUsername = $b->getUsername();
+
+            if (!is_string($aUsername)) {
+                return 1;
+            }
+
+            if (!is_string($bUsername)) {
+                return -1;
+            }
+
+            return strcmp($aUsername, $bUsername);
+        });
+
+        return $users;
     }
 }
---
+++ src/Form/CreateUserType.php
@@ -0,0 +1,116 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the MediaEase project.
+ *
+ * (c) Thomas Chauveau <contact.tomc@yahoo.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\Group;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints\PasswordStrength;
+use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
+
+final class CreateUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $formBuilder, array $options): void
+    {
+        $formBuilder
+            ->add('username', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a username',
+                    ]),
+                    new Length([
+                        'min' => 4,
+                        'minMessage' => 'Your username should be at least {{ limit }} characters',
+                        'max' => 30,
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'options' => [
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'first_options' => [
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Please enter a password',
+                        ]),
+                        new Length([
+                            'min' => 12,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 30,
+                        ]),
+                        new PasswordStrength(),
+                        new NotCompromisedPassword(),
+                    ],
+                    'label' => 'Password',
+                    'help' => 'Your password should be at least 12 characters long',
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                    'help' => 'Please repeat your password',
+                ],
+                'invalid_message' => 'The password fields must match.',
+                // Instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+            ])
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter an email',
+                    ]),
+                    new Length([
+                        'min' => 4,
+                        'minMessage' => 'Your email should be at least {{ limit }} characters',
+                        'max' => 30,
+                    ]),
+                ],
+            ])
+            ->add('group', EntityType::class, [
+                'class' => Group::class,
+                'choice_label' => 'name',
+            ])
+            ->add('save', ButtonType::class, [
+                'label' => 'Create',
+                'icon_before' => 'lucide:user-plus',
+                'button_class' => 'iconed-button bg-green-500 hover:bg-green-700 text-white font-bold pl-3 rounded h-[2.5rem] pr-[1.25rem]',
+                'icon_class' => 'w-6 h-6 fill-white button-icon',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $optionsResolver): void
+    {
+        $optionsResolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
